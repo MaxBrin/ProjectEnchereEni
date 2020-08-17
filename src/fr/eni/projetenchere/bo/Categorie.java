e36505dd23cf824361742144adde2d92648d287d
PINOT44 <68584634+PINOT44@users.noreply.github.com>
2020-08-17 14:24:58 +0200

    create classes
    classes utilisateur,articleVendu, Retrait, Categorie, Enchere

---
+++ src/fr/eni/projetenchere/bo/Categorie.java
@@ -0,0 +1,68 @@
+package fr.eni.projetenchere.bo;
+
+import java.io.Serializable;
+
+public class Categorie implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+		private int noCategorie;
+		private String libelle;
+		
+		
+		public Categorie() {
+			super();
+		}
+
+
+		public Categorie(String libelle) {
+			super();
+			this.libelle = libelle;
+		}
+
+
+		public Categorie(int noCategorie, String libelle) {
+			super();
+			this.noCategorie = noCategorie;
+			this.libelle = libelle;
+		}
+
+
+		public int getNoCategorie() {
+			return noCategorie;
+		}
+
+
+		public void setNoCategorie(int noCategorie) {
+			this.noCategorie = noCategorie;
+		}
+
+
+		public String getLibelle() {
+			return libelle;
+		}
+
+
+		public void setLibelle(String libelle) {
+			this.libelle = libelle;
+		}
+
+
+		public static long getSerialversionuid() {
+			return serialVersionUID;
+		}
+
+
+		@Override
+		public String toString() {
+			return "Categorie [noCategorie=" + noCategorie + ", libelle=" + libelle + "]";
+		}
+		
+		
+		
+	
+	
+	
+}
