20c8b4c20cc8e442d6c30f45ab81cbd64cd8546b
Maxime Brin <68320961+MaxBrin@users.noreply.github.com>
2020-08-18 11:21:44 +0200

    UPDATE ServletCreationCompte

--- src/fr/eni/projetenchere/ihm/ServletVersCreationCompte.java
+++ src/fr/eni/projetenchere/ihm/ServletVersCreationCompte.java
@@ -1,7 +1,6 @@
 package fr.eni.projetenchere.ihm;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,6 +8,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.validator.routines.EmailValidator;
 
 import fr.eni.projetenchere.bll.BLLException;
 import fr.eni.projetenchere.bll.UtilisateurMgr;
@@ -20,34 +22,65 @@ import fr.eni.projetenchere.bo.Utilisateur;
 @WebServlet("/CreationCompte")
 public class ServletVersCreationCompte extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-  
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/creationProfil.jsp");
 		rd.forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		boolean utilisateurValid = true;
 		String pseudo = request.getParameter("pseudo");
 		String nom = request.getParameter("nom");
 		String prenom = request.getParameter("prenom");
 		String email = request.getParameter("email");
+		EmailValidator validator = EmailValidator.getInstance();
+		if (!(validator.isValid(email))) {
+			utilisateurValid = false;
+			String emailNonValide = "L'email n'est pas valide";
+			request.setAttribute("messageErreur", emailNonValide);
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/creationProfil.jsp");
+			rd.forward(request, response);
+		}
 		String telephone = request.getParameter("telephone");
+		if (!(StringUtils.isNumeric(telephone)) || telephone.length() > 10 || telephone.length() < 10) {
+			utilisateurValid = false;
+			String telephoneNonValide = "Le numero de telephone n'est pas valide";
+			request.setAttribute("messageErreur", telephoneNonValide);
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/creationProfil.jsp");
+			rd.forward(request, response);
+		}
 		String rue = request.getParameter("rue");
 		String codePostal = request.getParameter("codePostal");
+		if (!(StringUtils.isNumeric(codePostal)) || (!(codePostal.length() == 5))) {
+			utilisateurValid = false;
+			String codePostalNonValide = "Le code Postal n'est pas valide";
+			request.setAttribute("messageErreur", codePostalNonValide);
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/creationProfil.jsp");
+			rd.forward(request, response);
+		}
 		String ville = request.getParameter("ville");
 		String motDePasse = request.getParameter("motDePasse");
 		String confirmationMdp = request.getParameter("confirmerMotDePasse");
-		
-		String mdpNonValide = "";
-		Utilisateur utlisateur;
-		
-		if(motDePasse.equals(confirmationMdp)) {
-			Utilisateur utilisateur = new Utilisateur(pseudo, nom, prenom, email, telephone, rue, codePostal, ville, motDePasse, 100, false);
+
+		if (!(motDePasse.equals(confirmationMdp))) {
+			utilisateurValid = false;
+			String mdpNonValide = "Les deux mots de passe saisis ne sont pas identiques";
+			request.setAttribute("messageErreur", mdpNonValide);
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/creationProfil.jsp");
+			rd.forward(request, response);
+		}
+
+		if (utilisateurValid) {
+			Utilisateur utilisateur = new Utilisateur(pseudo, nom, prenom, email, telephone, rue, codePostal, ville,
+					motDePasse, 100, false);
+
 			try {
 				UtilisateurMgr.ajoutUtilisateur(utilisateur);
 			} catch (BLLException e) {
@@ -55,18 +88,8 @@ public class ServletVersCreationCompte extends HttpServlet {
 			}
 			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/pageAccueil.jsp");
 			rd.forward(request, response);
-		} else {
-			mdpNonValide = "Les deux mots de passe saisis ne sont pas identiques";
-			request.setAttribute("messageErreur", mdpNonValide);
-			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/creationProfil.jsp");
-			rd.forward(request, response);
 		}
-		
-		
-		
-		
-		
-		
+
 	}
 
 }
