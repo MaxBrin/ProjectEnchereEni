4ac571ffb7b36b94f6cd5413d789c97f7749e6ad
MaxBrin <68320961+MaxBrin@users.noreply.github.com>
2020-08-18 22:52:05 +0200

    Update Creation Profil

--- src/fr/eni/projetenchere/bll/UtilisateurMgr.java
+++ src/fr/eni/projetenchere/bll/UtilisateurMgr.java
@@ -2,12 +2,15 @@ package fr.eni.projetenchere.bll;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.validator.routines.EmailValidator;
 
 import fr.eni.projetenchere.bo.Utilisateur;
 import fr.eni.projetenchere.dal.DALException;
 import fr.eni.projetenchere.dal.DAOFactory;
 import fr.eni.projetenchere.dal.UtilisateurDAO;
-
 
 public class UtilisateurMgr {
 
@@ -65,4 +68,101 @@ public class UtilisateurMgr {
 		return utilisateur;
 	}
 
+	public static String verifUtilisateur(Utilisateur utilisateur) {
+		StringBuilder erreur = new StringBuilder();
+		// TEST données saisie
+
+		// Pseudo
+		if (utilisateur.getPseudo() == null || StringUtils.isAlphanumeric(utilisateur.getPseudo())
+				|| utilisateur.getPseudo().trim().equals("")) {
+			erreur.append("PseudoNonValide");
+		}
+
+		// Nom
+		if (utilisateur.getNom() == null || utilisateur.getNom().trim().equals("")) {
+			erreur.append("Nom");
+		}
+
+		// Prenom
+		if (utilisateur.getPrenom() == null || utilisateur.getPrenom().trim().equals("")) {
+			erreur.append("Prenom");
+		}
+
+		// Email
+		EmailValidator validator = EmailValidator.getInstance();
+		if (!(validator.isValid(utilisateur.getEmail()))) {
+			erreur.append("EmailNonValide");
+		}
+
+		// Test si Pseudo et Email déjà présent
+
+		// Récuperation de la liste des utilisateurs
+		List<Utilisateur> listUtilisateur = new ArrayList<Utilisateur>();
+		try {
+			listUtilisateur = UtilisateurMgr.getListUtilisateur();
+		} catch (BLLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// Vérification dans cette liste pour voir si le pseudo ou l'email est déjà
+		// présent
+		for (Utilisateur utilisateurBD : listUtilisateur) {
+			if (utilisateurBD.getPseudo().equals(utilisateur.getPseudo())) {
+				erreur.append("PseudoPresent");
+			}
+			if (utilisateurBD.getEmail().equals(utilisateur.getEmail())) {
+				erreur.append("EmailPresent");
+			}
+		}
+
+		// Téléphone
+		if (!(StringUtils.isNumeric(utilisateur.getTelephone())) || (!(utilisateur.getTelephone().length() == 10))) {
+			erreur.append("Telephone");
+
+		}
+
+		// Rue
+		if (utilisateur.getRue() == null || utilisateur.getRue().trim().equals("")) {
+			erreur.append("Rue");
+		}
+
+		// CodePostal
+		if (!(StringUtils.isNumeric(utilisateur.getCodePostal())) || (!(utilisateur.getCodePostal().length() == 5))) {
+			erreur.append("CodePostal");
+		}
+
+		// Ville
+		if (utilisateur.getVille() == null || utilisateur.getVille().trim().equals("")) {
+			erreur.append("Ville");
+		}
+		// Test si les mot de passe sont identiquent
+		if (utilisateur.getMotDePasse() == null) {
+			erreur.append("MotDePasseIdentique");
+
+		} else if (!(isLegalPassword(utilisateur.getMotDePasse())) || (utilisateur.getMotDePasse().length() < 8)) {
+			erreur.append("MotDePasseVerif");
+		}
+
+		return erreur.toString();
+	}
+
+	// Pattern pour le password et vérifier qu'il conttient au moins une
+	// majuscule,une miniscule,une caractère spéciaux et un chiffre
+	private static final Pattern[] passwordRegexes = new Pattern[4];
+	static {
+		passwordRegexes[0] = Pattern.compile(".*[A-Z].*");
+		passwordRegexes[1] = Pattern.compile(".*[a-z].*");
+		passwordRegexes[2] = Pattern.compile(".*\\d.*");
+		passwordRegexes[3] = Pattern.compile(".*[~!].*");
+	}
+
+	// Vérification que le mot de passe correspond au pattern
+	public static boolean isLegalPassword(String pass) {
+
+		for (int i = 0; i < passwordRegexes.length; i++) {
+			if (!passwordRegexes[i].matcher(pass).matches())
+				return false;
+		}
+		return true;
+	}
 }
