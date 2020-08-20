18c6298aa9647aefc7c3da5851a255a6e4150bc6
adeline8444 <68584539+adeline8444@users.noreply.github.com>
2020-08-20 18:21:51 +0200

    Commit

--- src/fr/eni/projetenchere/ihm/ServletDetailVente.java
+++ src/fr/eni/projetenchere/ihm/ServletDetailVente.java
@@ -12,22 +12,27 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ServletDetailVente
  */
-@WebServlet("/ServletDetailVente")
+@WebServlet("/DetailVente")
 public class ServletDetailVente extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/detailVente.jsp");
 		rd.forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/detailVente.jsp");
 		rd.forward(request, response);
 	}
